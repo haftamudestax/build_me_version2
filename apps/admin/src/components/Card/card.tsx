49d218eb0fe47b6573663372db2ff2fc8810835d
haftamudestax <haftamudestax@gmail.com>
2026-08-12 05:23:40 -0700

    Fix bugs

--- apps/admin/src/components/Card/card.tsx
+++ apps/admin/src/components/Card/card.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export const card = () => {
   return <div>card</div>;
 };
--- apps/admin/src/routes/Routes.tsx
+++ apps/admin/src/routes/Routes.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Routes = () => {
   return <div>Routes</div>;
 };
